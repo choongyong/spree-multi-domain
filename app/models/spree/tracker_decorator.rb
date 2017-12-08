e100b925fb35905779a615f8c647ac8a037ac947
Koh Choong Yong <choongyong.koh@gmail.com>
2017-12-08 13:42:22 +0800

    Need to also handle the case where there is no domain passed in

--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -1,7 +1,9 @@
 Spree::Tracker.class_eval do
   belongs_to :store
 
-  def self.current(domain)
+  #Taken from https://github.com/spree-contrib/spree-multi-domain/issues/104
+  def self.current(domain = nil)
+    domain ||= Spree::Store.current.url
     Spree::Tracker.where(active: true).joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
   end
 end
